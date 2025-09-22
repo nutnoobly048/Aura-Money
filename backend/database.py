efc41377a75840edf20431d937c5804cd6dd353b
ChayakornAree <68070026@kmitl.ac.th>
2025-09-22 11:58:53 +0700

    UPDATE Check user exist when regis

--- backend/database.py
+++ backend/database.py
@@ -38,17 +38,31 @@ def get_data():
     except Exception as err:
         raise RuntimeError(f"Database error: {err}")
 
+# check user is exist?
+def check_user(email):
+    try:
+        db = ConnectorMysql()
+        cursor = db.cursor()
+        cursor.execute("SELECT * WHERE email=%s", (email,))
+        user = cursor.fetchone()
+        cursor.close()
+        return user
+    except Exception as err:
+        raise RuntimeError(f"Database error: {err}")
+
 # method register with username , password (encrypt with bcrypt function) and email
 def register_db(username , password , email):
     try:
         db = ConnectorMysql()
         cursor = db.cursor()
-        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()) # encrypt password code
-        stmt = "INSERT INTO user (username , password , email) VALUES (%s, %s, %s)"
-        payload = (username , hashed , email)
-        cursor.execute(stmt,payload)
-        db.commit()
-        return jsonify({"success": "Registered successfull"}), 200
+        if not check_user(email):
+            hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()) # encrypt password code
+            stmt = "INSERT INTO user (username , password , email) VALUES (%s, %s, %s)"
+            payload = (username , hashed , email)
+            cursor.execute(stmt,payload)
+            db.commit()
+            return jsonify({"success": "Registered successfull"}), 200
+        return jsonify({"errror": "Already has user"}), 401
     except Exception as err:
         raise RuntimeError(f"Database error: {err}")
 
