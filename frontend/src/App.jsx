2f0f46d5755c5558b5fb953b0f88656f2097d4b9
Guy-Jirasin <68070015@kmitl.ac.th>
2025-10-07 16:12:16 +0700

    setting react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,31 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="h-screen flex bg-gradient-to-br from-[#62b79c] to-[#afd1a1]">
-      {/*------------------left bar section----------------*/}
-      <div className="h-screen flex flex-col w-40">
-        <img src='/logo.png' alt='logo' className='w-32 h-auto m-4 self-start'/>
-        <div className='flex flex-col flex-1 items-center justify-center text-2xl gap-10 mb-30'>
-          <h2 className='border-5 border-white px-3 py-2 rounded-3xl text-white'>Sign up</h2>
-          <h2 className='bg-white px-6 py-2 rounded-3xl text-teal-600'>Login</h2>
-        </div>
-      </div>
-      {/*------------------left bar section----------------*/}
-      {/*------------------right big text----------------*/}
-      <div className='h-full relative flex-1 overflow-auto bg-white'>
-          <img src='/home-bg.png' alt='home-bg' className='min-h-2/4 absolute top-0 right-0 object-cover'/>
-          <div className='h-6/10 w-full flex-wrap bg-white absolute bottom-0 right-0
-          items-centerborder-white rounded-3xl flex justify-center items-center'>
-            <div className='flex flex-col m-10'>
-              <h2 className='text-4xl font-bold'>What is <span className='text-[#70aa82]'>Aura Money ?</span></h2>
-              <p className='text-2xl '>An idea for an auto money-saving visualizer and planner is proposed,
-              emphasizing its importance, though specific reasons and supporting data are yet to be provided.</p>
-              <img src='/home-money.png' alt='home-money' className='w-30 h-auto'/>
-            </div>
-          </div>
-      </div>
-      {/*------------------right big text----------------*/}
-    </div>
+    <div className="">Dashboard App</div>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Landingpage from './components/landingpage.jsx'
+import Login from './components/login.jsx'
+import Register from './components/register.jsx'
+import Profile from './components/profile.jsx'
+
+const router = createBrowserRouter([
+  {path:"/", element:<App />},
+  {path:"/landingpage", element:<Landingpage />},
+  {path:"/login", element:<Login />},
+  {path:"/register",element:<Register />},
+  {path:"/profile", element:<Profile/>}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
