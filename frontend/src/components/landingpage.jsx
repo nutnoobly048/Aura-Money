92a58bd5fa350f2069f04dcc549c9ecfe0753475
Guy-Jirasin <68070015@kmitl.ac.th>
2025-10-08 10:51:36 +0700

    refactor landing page by building mobile first

--- frontend/src/components/landingpage.jsx
+++ frontend/src/components/landingpage.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
+import '../../src/App.css'
 
 function landingpage() {
   return (
-    <div className="h-screen flex bg-gradient-to-br from-[#62b79c] to-[#afd1a1]">
-      {/*------------------left bar section----------------*/}
-      <div className="h-screen flex flex-col w-40">
-        <img src='/logo.png' alt='logo' className='w-32 h-auto m-4 self-start'/>
-        <div className='flex flex-col flex-1 items-center justify-center text-2xl gap-10 mb-30'>
-          <h2 className='border-5 border-white px-3 py-2 rounded-3xl text-white'>Sign up</h2>
-          <h2 className='bg-white px-6 py-2 rounded-3xl text-teal-600'>Login</h2>
+    <div className="flex flex-col w-screen h-screen bg-gradient-to-br from-[#62b79c] to-[#afd1a1]">
+      <nav className='flex justify-between items-center p-3'>
+        <img src='logo.png' className='w-25 h-15'/>
+        <div className='flex space-x-5'>
+          <button className='border rounded-3xl border-white text-white px-3 py-1'>Signup</button>
+          <button className='rounded-3xl bg-white text-[#62b79c] px-3 py-1'>Login</button>
         </div>
+      </nav>
+      <div className='flex flex-col justify-center items-center bg-white py-25 px-5 m-5 rounded-2xl'>
+        <img src='home-money.png' className='w-[10vh]'/>
+        <p className='font-bold text-xl'>What is</p>
+        <p className='text-[#62b79c] font-bold text-3xl'>Aura Money?</p>
+        <p className='text-center'>An idea for an auto money-saving visualizer and planner is proposed, emphasizing its importance, 
+          though specific reasons and supporting data are yet to be provided.</p>
       </div>
-      {/*------------------left bar section----------------*/}
-      {/*------------------right big text----------------*/}
-      <div className='h-full relative flex-1 overflow-auto bg-white'>
-          <img src='/home-bg.png' alt='home-bg' className='min-h-2/4 absolute top-0 right-0 object-cover'/>
-          <div className='h-6/10 w-full flex-wrap bg-white absolute bottom-0 right-0
-          items-centerborder-white rounded-3xl flex justify-center items-center'>
-            <div className='flex flex-col m-10'>
-              <h2 className='text-4xl font-bold'>What is <span className='text-[#70aa82]'>Aura Money ?</span></h2>
-              <p className='text-2xl '>An idea for an auto money-saving visualizer and planner is proposed,
-              emphasizing its importance, though specific reasons and supporting data are yet to be provided.</p>
-              <img src='/home-money.png' alt='home-money' className='w-30 h-auto'/>
-            </div>
-          </div>
-      </div>
-      {/*------------------right big text----------------*/}
     </div>
   )
 }
