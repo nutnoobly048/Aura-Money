881806a297858f4df17e249df60d11ec3786eb91
Guy-Jirasin <68070015@kmitl.ac.th>
2025-10-14 16:21:32 +0700

    add landing page animation

--- frontend/src/components/landingpage.jsx
+++ frontend/src/components/landingpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../src/App.css'
+import { Link } from "react-router-dom";
 
 function landingpage() {
   return (
@@ -8,8 +9,11 @@ function landingpage() {
       <nav className='absolute top-0 left-0 w-full flex justify-between sm:justify-center! items-center p-3'>
         <img src='logo.png' className='w-[15vh] sm:w-[20vh]!'/>
         <div className='flex space-x-5 text-xl sm:hidden!'>
-          <button className='border rounded-3xl border-white text-white px-3 py-1'>Signup</button>
-          <button className='rounded-3xl bg-white text-[#62b79c] px-3 py-1'>Login</button>
+          <Link to='/register' className='border rounded-3xl border-white text-white px-3 py-1 cursor-pointer
+          hover:bg-white hover:text-ui-green1 hover:scale-110 transition duration-150 ease-in-out'>Signup</Link>
+          <Link to='/login' className='rounded-3xl bg-white text-ui-green1 px-3 py-1 cursor-pointer 
+          hover:bg-transparent hover:text-white hover:border hover:scale-110 hover:border-white 
+          transition duration-150 ease-in-out'>Login</Link>
         </div>
       </nav>
       <div className='flex flex-col justify-center items-center max-w-[640px] bg-white py-25 px-5 m-5 rounded-2xl gap-y-2 sm:hidden'>
@@ -20,8 +24,11 @@ function landingpage() {
           though specific reasons and supporting data are yet to be provided.</p>
       </div>
       <div className='hidden sm:flex! w-160 space-x-10 text-xl font-bold'>
-        <button className='flex-1 border rounded-3xl border-white text-white px-3 py-1'>Signup</button>
-        <button className='flex-1 rounded-3xl bg-white text-[#62b79c] px-3 py-1'>Login</button>
+        <Link to='/register' className='flex-1 border rounded-3xl border-white text-white text-center px-3 py-1 
+        cursor-pointer hover:bg-white hover:text-ui-green1 hover:scale-110 transition duration-150 ease-in-out'>Signup</Link>
+        <Link to='/login' button className='flex-1 rounded-3xl bg-white text-ui-green1 text-center px-3 py-1 
+        cursor-pointer hover:bg-transparent hover:text-white hover:border hover:scale-110 hover:border-white transition 
+        duration-150 ease-in-out'>Login</Link>
       </div>
     </div>
   )
