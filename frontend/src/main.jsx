a9688c61af19bebde1dd70d7c0bdcd78c587854d
Guy-Jirasin <68070015@kmitl.ac.th>
2025-10-21 13:07:11 +0700

    delete login, register page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Landingpage from './components/landingpage.jsx'
-import Login from './components/login.jsx'
-import Register from './components/register.jsx'
 import Profile from './components/profile.jsx'
 import Login_register from './components/login_register.jsx'
 
@@ -13,8 +11,6 @@ import Login_register from './components/login_register.jsx'
 const router = createBrowserRouter([
   {path:"/", element:<App />},
   {path:"/landingpage", element:<Landingpage />},
-  {path:"/login", element:<Login />},
-  {path:"/register",element:<Register />},
   {path:"/profile", element:<Profile/>},
   {path:"/login_register", element:<Login_register/>}
 ])
