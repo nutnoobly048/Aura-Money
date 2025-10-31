5d736bb1f2d6f3855f455c5c26eea6601a580d37
Guy-Jirasin <68070015@kmitl.ac.th>
2025-10-31 21:25:15 +0700

    start doing iore delete

--- frontend/src/components/DashBoardComponents/AccountBoard.jsx
+++ frontend/src/components/DashBoardComponents/AccountBoard.jsx
@@ -1,6 +1,7 @@
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
-import { useContext } from "react";
+import { ArrowBigLeft, ArrowBigRight, Trash } from "lucide-react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { APIContext } from "../APIProvider";
+import axios from "axios";
 
 export default function AccountBoard({ pageOpen }) {
   const { iore, setIore } = useContext(APIContext);
@@ -27,23 +28,57 @@ const HistoryBoard = ({ iore }) => {
   return (
     <div className="overflow-y-auto flex flex-col px-4 py-4 border-2 border-zinc-300 rounded-2xl space-y-2">
       {iore?.map((item) => (
-        <div
-          key={item?.track_id}
-          className="relative w-full flex justify-between items-center border-2 border-zinc-200 shadow-sm rounded-xl px-4 py-2  "
-        >
-          <p className="text-xl">{item?.category_name}</p>
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center">
-            <p>{item?.account_name}</p>
-            <p>{new Date(item?.date).toLocaleDateString("en-GB")}</p>
-          </div>
-          <p className={`${item?.types == "expense" ? "text-red-500" : "text-green-700"} text-xl`}>
-            {`${item?.types == "expense" ? "-" : ""}${item?.amount} ฿`}
-          </p>
-        </div>
+        <HistoryItem key={item?.track_id} item={item} />
       ))}
     </div>
   );
 };
+
+const HistoryItem = ({ item }) => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const elementArea = useRef(null);
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+    const handler = (e) => {
+      if (!elementArea.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    }
+  }, [isMenuOpen]);
+
+  const handleDeleteIore = async (e ,id) => {
+    await axios.post("http://localhost:5000/delete_iore", id);
+    console.log('deleted', id);
+  }
+
+  return (
+    <div
+      ref={elementArea}
+      key={item?.track_id}
+      onClick={() => setMenuOpen(true)}
+      className="relative w-full flex justify-between items-center border-2 border-zinc-200 shadow-sm rounded-xl px-4 py-2  "
+    >
+      {isMenuOpen && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black/60 flex items-center justify-center rounded-xl z-10">
+          <Trash onClick={() => handleDeleteIore(item?.track_id)} className="size-8 text-white bg-red-500 rounded-full p-1" />
+        </div>
+      )}
+      <p className="text-xl">{item?.category_name}</p>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center">
+        <p>{item?.account_name}</p>
+        <p>{new Date(item?.date).toLocaleDateString("en-GB")}</p>
+      </div>
+      <p className={`${item?.types == "expense" ? "text-red-500" : "text-green-700"} text-xl`}>
+        {`${item?.types == "expense" ? "-" : ""}${item?.amount} ฿`}
+      </p>
+    </div>
+  );
+}
 
 const SummationBoard = ({ iore }) => {
   const sumArray = (arr) => {let sum=0;arr?.forEach(item => {sum += item});return sum}
